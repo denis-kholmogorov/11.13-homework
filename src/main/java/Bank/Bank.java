52acfa8effd251864eadad73d3099d788f486c5f
kholmogorov.denis <kholmogorov.denis@gmail.com>
2019-12-16 18:03:50 +0300

    changed finish

--- src/main/java/Bank/Bank.java
+++ src/main/java/Bank/Bank.java
@@ -34,13 +34,57 @@ public class Bank {
      * счетов (как – на ваше усмотрение)
      */
     public void transfer(String fromAccountNum, String toAccountNum, long amount) {
-        if (amount < 50000) {
-            realize(fromAccountNum, toAccountNum, amount);
-        } else {
-            realizeWithCompare(fromAccountNum, toAccountNum, amount);
-        }
-    }
+        Account fromAccount = accounts.get(fromAccountNum);
+        Account toAccount = accounts.get(toAccountNum);
 
+        System.out.println("Клиент " + fromAccount.getAccNumber() + " с балансом "
+                + getBalance(fromAccountNum) + " пытается перевести деньги клиенту "
+                + toAccount.getAccNumber() + " с балансом " + getBalance(toAccountNum) + " сумму - " + amount);
+            try {
+                if (!fromAccount.isBlock() && !toAccount.isBlock()) {   // Проверка блокировки счетов
+                    fromAccount.reduceMoney(amount);                    // Проводим снятие денег
+                    toAccount.addMoney(amount);                         // Проводим пополнение денег
+                    if (amount > 50000) {                               // Проверка суммы
+                        fromAccount.setBlock(true);                     // Блокировка отправителя на время проверки
+                        toAccount.setBlock(true);                       // Блокировка принимающего
+                        System.out.println("Началась проверка счета клиента " + fromAccount.getAccNumber() +
+                                " и клиента " + toAccount.getAccNumber());
+                        boolean block = isFraud(fromAccount.getAccNumber(), toAccount.getAccNumber(), amount); // Проверка перевода
+                        if (!block) {                                                                          // Проверка результата проверки
+                            System.out.println("Проверка прошла успешно счета клиента " + fromAccount.getAccNumber()
+                                    + " с балансом " + getBalance(fromAccountNum) + " и клиента " + toAccount.getAccNumber()
+                                    + " с балансом " + getBalance(toAccountNum) + " разблокированы!");
+                            fromAccount.setBlock(false);        // Разблокировка счета отправителя
+                            toAccount.setBlock(false);          // Разблокировка счета принимающего
+                        } else {
+                            System.err.println("Счет клиента " + fromAccount.getAccNumber() + " с балансом "
+                                    + getBalance(fromAccountNum) + " и счет клиента  "
+                                    + toAccount.getAccNumber() + " с балансом " + getBalance(toAccountNum)
+                                    + " Заблокированы");
+                        }
+                    } else {
+                        System.out.println("УСПЕХ! Перевод успешно завершен от клиента " + fromAccount.getAccNumber()
+                                + " с балансом " + getBalance(fromAccountNum) + " клиенту "
+                                + toAccount.getAccNumber() + " с балансом " + getBalance(toAccountNum));
+                    }
+                }
+                else if (fromAccount.isBlock()) // проверка заблокированного счета
+                {
+                    System.err.println("СТОП! На данный момент клиент " + fromAccount.getAccNumber()
+                            + " заблокирован и не может перевести деньги " + toAccount.getAccNumber());
+                }
+                else if (toAccount.isBlock()) // проверка заблокированного счета
+                {
+                    System.err.println("СТОП! На данный момент клиент " + fromAccount.getAccNumber()
+                            + " не может перевести деньги т.к счет клиента " + toAccount.getAccNumber() + " заблокрован");
+                }
+
+            } catch (UnavailableQuantityException e) {
+                System.out.println(e.getMessage());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     /**
      * TODO: реализовать метод. Возвращает остаток на счёте.
@@ -52,75 +96,8 @@ public class Bank {
             }
         }
     }
-
-    public void realize(String fromAccountNum, String toAccountNum, long amount) {
-
-        Account fromAccount = accounts.get(fromAccountNum);
-        Account toAccount = accounts.get(toAccountNum);
-        System.out.println("Клиент " + fromAccount.getAccNumber() + " с балансом "
-                + getBalance(fromAccountNum) + " пытается перевести деньги клиенту "
-                + toAccount.getAccNumber() + " с балансом " + getBalance(toAccountNum) + " сумму - " + amount);
-        try {
-            if (!fromAccount.isBlock() && !toAccount.isBlock()) {
-                fromAccount.reduceMoney(amount);
-                toAccount.addMoney(amount);
-                System.out.println("УСПЕХ! Перевод успешно завершен от клиента " + fromAccount.getAccNumber()
-                        + " с балансом " + getBalance(fromAccountNum) + " клиенту "
-                        + toAccount.getAccNumber() + " с балансом " + getBalance(toAccountNum));
-            } else if (fromAccount.isBlock()) {
-                System.err.println("СТОП! На данный момент клиент " + fromAccount.getAccNumber()
-                        + " заблокирован и не может перевести деньги " + toAccount.getAccNumber());
-            } else if (toAccount.isBlock()) {
-                System.err.println("СТОП! На данный момент " + fromAccount.getAccNumber()
-                        + " не может перевести деньги т.к счет клиента " + toAccount.getAccNumber() + " заблокрован");
-            }
-        } catch (UnavailableQuantityException e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
-
-    public void realizeWithCompare(String fromAccountNum, String toAccountNum, long amount){
-        Account fromAccount = accounts.get(fromAccountNum);
-        Account toAccount = accounts.get(toAccountNum);
-        System.out.println("ПРОВЕРКА! Клиент " + fromAccount.getAccNumber() + " с балансом "
-                + getBalance(fromAccountNum) + " пытается перевести деньги клиенту "
-                + toAccount.getAccNumber() + " с балансом " + getBalance(toAccountNum)
-                + " сумму - " + amount + " необходима проверка");
-        try {
-            if (!fromAccount.isBlock() && !toAccount.isBlock()) {
-                fromAccount.reduceMoney(amount);
-                toAccount.addMoney(amount);
-                fromAccount.setBlock(true);
-                toAccount.setBlock(true);
-                System.out.println("Началась проверка счета клиента " + fromAccount.getAccNumber() +
-                        " и клиента " + toAccount.getAccNumber());
-                boolean block = isFraud(fromAccount.getAccNumber(), toAccount.getAccNumber(), amount);
-                if (!block) {
-                    System.out.println("Проверка прошла успешно счета клиента " + fromAccount.getAccNumber()
-                            + " с балансом " + getBalance(fromAccountNum) + " и клиента " + toAccount.getAccNumber()
-                            + " с балансом " + getBalance(toAccountNum) + " разблокированы!");
-                    fromAccount.setBlock(false);
-                    toAccount.setBlock(false);
-                } else {
-                    System.err.println("Счет клиента " + fromAccount.getAccNumber() + " с балансом "
-                            + getBalance(fromAccountNum) + " и счет клиента  "
-                            + toAccount.getAccNumber() + " с балансом " + getBalance(toAccountNum)
-                            + " Заблокированы");
-                }
-
-            } else if (fromAccount.isBlock()) {
-                System.out.println("На данный момент счет клиента " + fromAccount.getAccNumber() + " заблокирован");
-            } else if (toAccount.isBlock()) {
-                System.out.println("На данный момент счет клиента " + toAccount.getAccNumber() + " заблокрован");
-            }
-        } catch (UnavailableQuantityException e) {
-            System.out.println(e.getMessage());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
+
+
 
 
